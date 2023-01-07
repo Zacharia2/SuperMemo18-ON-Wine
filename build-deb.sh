d442bd41c1d46e43c9189b393bd2c02353c85bf1
WhiteFall <32425955+Zacharia2@users.noreply.github.com>
2023-01-07 22:04:10 +0800

    Create build-deb.sh

---
+++ build-deb.sh
@@ -0,0 +1,14 @@
+
+version=1.1.0
+product_name=yourapp_name
+project_name=yourproduct_name
+package_name=yourpackage_name
+export build_version=$version.$BUILD_NUMBER
+export target_name=${product_name}_${build_version}_arm64.deb
+export build_target=$WORKSPACE/${target_name}
+ 
+cd $WORKSPACE
+ 
+chmod +x deploy.sh
+chmod +x debPackage/DEBIAN/postinst debPackage/DEBIAN/postrm debPackage/DEBIAN/prerm debPackage/DEBIAN/preinst
+./deploy.sh $build_version $product_name $package_name $project_name
