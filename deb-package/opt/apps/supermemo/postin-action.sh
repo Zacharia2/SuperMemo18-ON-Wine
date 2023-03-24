c1b00076909cd3ff4bf2296e597546ef686e7b4d
WhiteFall <32425955+Zacharia2@users.noreply.github.com>
2023-03-24 21:00:42 +0800

    Update postin-action.sh

--- deb-package/opt/apps/supermemo/postin-action.sh
+++ deb-package/opt/apps/supermemo/postin-action.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-u-name=`eval echo $(who | grep "(:0)" | awk '{print $1}')`
-current-dir=`eval echo "/home/"${u-name}"/桌面/SuperMemo卡组与资料库/"`
+sname=`eval echo $(who | grep "(:0)" | awk '{print $1}')`
+scurrent=`eval echo "/home/"${sname}"/桌面/SuperMemo卡组与资料库/"`
 
 # 创建父目录
 mkdir -p /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_c/SuperMemo/SuperMemo资料库
-mkdir -p $current-dir
+mkdir -p $scurrent
 # 给予SuperMemo资料库所有用户读写权限
 sudo chmod a+rw /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_c/SuperMemo/SuperMemo资料库
-sudo chmod a+rw $current-dir
+sudo chmod a+rw $scurrent
 
 # 添加C盘、D盘、E盘卡包文件夹与资料库，应放到明显的位置打开。
-ln -s /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_c/SuperMemo/systems ${current-dir}"SuperMemo_C盘卡组"
-ln -s /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_d/ ${current-dir}"SuperMemo_D盘卡组"
-ln -s /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_e/ ${current-dir}"SuperMemo_E盘卡组"
-ln -s /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_c/SuperMemo/SuperMemo资料库 ${current-dir}"SuperMemo_C盘资料库"
+ln -s /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_c/SuperMemo/systems ${scurrent}"SuperMemo_C盘卡组"
+ln -s /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_d/ ${scurrent}"SuperMemo_D盘卡组"
+ln -s /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_e/ ${scurrent}"SuperMemo_E盘卡组"
+ln -s /opt/apps/supermemo/SM18.05-WINE-Vessel/drive_c/SuperMemo/SuperMemo资料库 ${scurrent}"SuperMemo_C盘资料库"
